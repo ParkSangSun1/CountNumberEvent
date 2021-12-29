914ca1c415dd13cbc0fcdb5d29f8e1cf32fb3190
ParkSangSun <appleasz1028102@gmail.com>
2021-12-29 17:41:46 +0900

    Add : AfterEvaluate

--- app/src/main/java/com/pss/countnumberevent/MainActivity.kt
+++ app/src/main/java/com/pss/countnumberevent/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pss.countnumberevent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.pss.library.CountNumberEvent
 import com.pss.library.CountNumberEvent.Companion.countNumber
 
 class MainActivity : AppCompatActivity() {
